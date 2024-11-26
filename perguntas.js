582923b0cbd451e3fa9f2e5a3357babbf0ef15cc
Arthur Silva <168222534+thurinkz@users.noreply.github.com>
2024-11-25 21:16:29 -0300

    carlinhos

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'Python é uma linguagem de programação versátil, conhecida por sua simplicidade e legibilidade.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Espanha?',
+    'A capital da Espanha é Madrid.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma variável?',
+    'Uma variável é um espaço na memória usado para armazenar valores durante a execução de um programa.'
+)
+
+criaCartao(
+    'Língua Inglesa',
+    'Como se diz adeus em inglês?',
+    'Adeus em inglês é "Goodbye".'
+)
